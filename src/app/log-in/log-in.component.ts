e025acac36d09c7ab3f5a8ecfabf230bd48780b7
Dimitrios Kapetanis <dimitrioskapetanis1996@gmail.com>
2024-05-11 11:16:05 +0200

    added log-in-button from material

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-log-in',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatButtonModule, MatIconModule],
   templateUrl: './log-in.component.html',
-  styleUrl: './log-in.component.scss'
+  styleUrl: './log-in.component.scss',
 })
-export class LogInComponent {
-
-}
+export class LogInComponent {}
