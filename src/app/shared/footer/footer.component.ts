4c372dbfdde8c0c79aeccfac93f56739d3ca094b
Dimitrios Kapetanis <dimitrioskapetanis1996@gmail.com>
2024-05-25 23:27:44 +0200

    changed redirection-button style

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { PreorderdataService } from '../firebase-services/preorderdata.service';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
+  /* constructor(private preOrderService: PreorderdataService) {}
 
+  totalAmount = this.preOrderService.totalItemAmount; */
 }
