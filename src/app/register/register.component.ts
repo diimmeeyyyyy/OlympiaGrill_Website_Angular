bfdc240581ac76d0df934dd2e072b072df139d86
Dimitrios Kapetanis <dimitrioskapetanis1996@gmail.com>
2024-06-03 10:25:20 +0200

    display password requirements when onFocus password inputfield

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,77 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  name!: string;
+  email!: string;
+  password!: string;
+  confirmPassword!: string;
+  acceptTerms = false;
+  showPasswordRequirements = false;
 
+  checkRegisterRequirements() {
+    if (!this.allInputfieldsHaveValue()) {
+      return false;
+    }
+    if (!this.termsWereAccepted()) {
+      return false;
+    }
+    if (!this.passwordRequirementsAreFulfilled()) {
+      return false;
+    }
+    if (!this.passwordsMatch()) {
+      return false;
+    }
+    return true;
+  }
+
+  allInputfieldsHaveValue() {
+    return this.name || this.email || this.password || this.confirmPassword;
+  }
+
+  termsWereAccepted() {
+    return this.acceptTerms;
+  }
+
+  passwordRequirementsAreFulfilled() {
+    return (
+      this.passwordIsLongEnough() ||
+      this.passwordHasCapitalLetter() ||
+      this.passwordContainsNumber()
+    );
+  }
+
+  passwordsMatch(){
+    return this.password === this.confirmPassword
+  }
+
+  onFocusPassword() {
+    this.showPasswordRequirements = true;
+  }
+
+  onBlurPassword() {
+    this.showPasswordRequirements = false;
+  }
+
+  passwordIsLongEnough() {
+    return this.password && this.password.length >= 10;
+  }
+
+  passwordHasCapitalLetter() {
+    const capitalLetter = /[A-Z]/;
+    return this.password && capitalLetter.test(this.password);
+  }
+
+  passwordContainsNumber() {
+    const number = /[0-9]/;
+    return this.password && number.test(this.password);
+  }
 }
